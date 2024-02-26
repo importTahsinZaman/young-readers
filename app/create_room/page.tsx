fb6ab02ca8ee1a0d05f1306e967c35782b2e46f2
importTahsinZaman <tahsinz21366@gmail.com>
2024-02-26 15:32:18 -0500

    created create_room form

--- app/create_room/page.tsx
+++ app/create_room/page.tsx
@@ -1,8 +1,67 @@
-import { createClient } from "@/utils/supabase/server";
+"use client";
 
-export default async function create_room() {
-  const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
+import { useForm, SubmitHandler } from "react-hook-form";
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+type Inputs = {
+  theme: string;
+  playerCount: number;
+  loopCount: number;
+  gradeLevel: number;
+};
+
+export default function create_room() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch("theme")); // watch input value by passing the name of it
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+      <label>theme</label>
+      <input {...register("theme", { required: true })} />
+      {errors.theme && <span>Theme is required</span>}
+
+      <label>Number of Players:</label>
+      <input
+        type="number"
+        step="1"
+        max={5}
+        min={2}
+        defaultValue={2}
+        {...register("playerCount", { required: true })}
+      />
+      {errors.playerCount && <span>Player count is required</span>}
+
+      <label>Number of Loops:</label>
+      <input
+        type="number"
+        step="1"
+        max={5}
+        min={2}
+        defaultValue={2}
+        {...register("loopCount", { required: true })}
+      />
+      {errors.loopCount && <span>Loop count is required</span>}
+
+      <label>Grade Level:</label>
+      <input
+        type="number"
+        step="1"
+        max={5}
+        min={2}
+        defaultValue={2}
+        {...register("gradeLevel", { required: true })}
+      />
+      {errors.gradeLevel && <span>Grade level is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
 }
