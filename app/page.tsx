1e5cc99af497c296b000ed508276810d525a62b2
importTahsinZaman <tahsinz21366@gmail.com>
2024-02-26 14:36:10 -0500

    created pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
@@ -28,27 +29,24 @@ export default async function Index() {
         </div>
       </nav>
 
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
+      <div className="flex flex-col">
+        <h1 className="text-xl">Username:</h1>
+        <input type="text" id="username" name="username"></input>
+        <div className="flex flex-row">
+          <Link
+            href="/create_room"
+            className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
           >
-            Supabase
-          </a>
-        </p>
-      </footer>
+            Create Room
+          </Link>
+          <Link
+            href="/join_room"
+            className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+          >
+            Join Room
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
