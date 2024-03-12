88991ae8a092a9ff74a5794ed3dd0cbae371342c
importTahsinZaman <tahsinz21366@gmail.com>
2024-03-11 22:08:22 -0400

    created fetching room for manage room

--- app/manage_room/[game_code]/page.tsx
+++ app/manage_room/[game_code]/page.tsx
@@ -5,13 +5,24 @@ import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function manage_room() {
+  const supabase = createClient();
   const pathname = usePathname();
   const [gamecode, setGamecode] = useState("");
+  const [notes, setNotes] = useState<any[] | null>(null);
 
   useEffect(() => {
     const slug = pathname.substring(pathname.lastIndexOf("/") + 1);
     setGamecode(slug);
+
+    const getData = async () => {
+      const { data } = await supabase
+        .from("stories")
+        .select("*")
+        .eq("game_code", slug);
+      setNotes(data);
+    };
+    getData();
   }, [pathname]);
 
-  return <></>;
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
 }
