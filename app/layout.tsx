0648869a2940f92c7f9538c5d0d14cceed238e6b
importTahsinZaman <tahsinz21366@gmail.com>
2024-03-17 18:18:24 -0400

    new icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Young Readers",
+  title: "skytales",
   description: "The multiplayer choose your own adventure story book!",
 };
 
